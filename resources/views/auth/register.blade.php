0a2deb483c75de1a33e25778f1afaa8b4075b6bc
unknown <majiar16@gmail.com>
2020-01-19 21:33:01 -0500

    cuando se loguea redireciona a registrar con un feedback

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
+                        @include('includes.message')
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('nombres') }}</label>
 
